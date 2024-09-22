d69236e82f227d39aa1ef2a6387f580ba9ec24ef
Zucker-jex <jex.zucker@outlook.com>
2024-09-23 00:43:12 +0800

    24-09-23

---
+++ radio.bash
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+LIST_FILE=~/radio.lis
+
+# 检查是否有默认电台列表文件，如果没有则创建
+if [ ! -f "$LIST_FILE" ]; then
+    echo "[Gensokyo Radio]https://stream.gensokyoradio.net/3" > "$LIST_FILE"
+    echo "[Vocaloid Radio]http://curiosity.shoutca.st:8019/stream" >> "$LIST_FILE"
+fi
+
+# 读取电台列表并显示
+read_radio_list() {
+    mapfile -t radios < "$LIST_FILE"
+}
+
+# 显示电台列表的菜单
+draw_menu() {
+    tput clear
+    echo "=== 电台列表 ==="
+    for i in "${!radios[@]}"; do
+        if [ "$i" -eq "$selected" ]; then
+            tput setaf 3 # 设置黄色字体
+            echo "> $(echo "${radios[$i]}" | cut -d']' -f1 | tr -d '[')"
+            tput sgr0 # 重置颜色
+        else
+            echo "  $(echo "${radios[$i]}" | cut -d']' -f1 | tr -d '[')"
+        fi
+    done
+    echo "================"
+    echo "J/K:上下选择 A:添加电台 Q:退出"
+}
+
+# 添加电台
+add_radio() {
+    echo "请输入电台标题:"
+    read -r title
+    echo "请输入电台URL:"
+    read -r url
+    echo "[$title]$url" >> "$LIST_FILE"
+    echo "电台已添加: $title"
+    read_radio_list
+}
+
+# 播放电台
+play_radio() {
+    selected_radio="${radios[$selected]}"
+    radio_name=$(echo "$selected_radio" | cut -d']' -f1 | tr -d '[')
+    radio_url=$(echo "$selected_radio" | cut -d']' -f2)
+
+    echo "正在播放: $radio_name"
+
+    # 使用 mpv 播放
+    mpv --no-video "$radio_url"
+}
+
+read_radio_list
+selected=0
+while true; do
+    draw_menu
+
+    read -rsn1 key
+
+    case "$key" in
+        $'\x1B')
+            read -rsn2 key
+            if [[ "$key" == "[A" || "$key" == "OA" || "$key" == "k" ]]; then # 向上箭头或k
+                ((selected--))
+                if [ "$selected" -lt 0 ]; then
+                    selected=$((${#radios[@]} - 1))
+                fi
+            elif [[ "$key" == "[B" || "$key" == "OB" || "$key" == "j" ]]; then # 向下箭头或j
+                ((selected++))
+                if [ "$selected" -ge "${#radios[@]}" ]; then
+                    selected=0
+                fi
+            fi
+            ;;
+        "")
+            play_radio
+            ;;
+        A|a)
+            add_radio
+            ;;
+        Q|q)
+            echo "再见！"
+            exit 0
+            ;;
+    esac
+done
